c3f4388ebcc0dac5f47794b14d7220a68dd09bc5
Dávid Karnok <akarnokd@gmail.com>
2018-03-28 23:39:39 +0200

    Update mod settings

--- AutoGatherTransport/items.lua
+++ AutoGatherTransport/items.lua
@@ -3,4 +3,9 @@ PlaceObj('ModItemCode', {
 	'name', "AutoGatherTransportScript",
 	'FileName', "Code/AutoGatherTransportScript.lua",
 }),
+PlaceObj('ModItemCode', {
+	'name', "AutoPathFinding",
+	'comment', "Common logic for enhanced zone-based pathfinding assist",
+	'FileName', "Code/AutoPathFinding.lua",
+}),
 }
